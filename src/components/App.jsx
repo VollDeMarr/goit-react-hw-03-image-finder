a9a1ef2a639691ac42997d2cd518861d55c02131
Volodymyr Bobanych <97664943+VollDeMarr@users.noreply.github.com>
2022-07-12 00:56:21 +0300

    щось трохи написав

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,22 @@
-import Searchbar from './Searchbar/Searchbar'
-import LoaderFn from './Loader/Loader'
-import Button from './Button/Button'
+// import ImageGallery from './ImageGallery/ImageGallery';
+import Searchbar from './Searchbar/Searchbar';
+// import Loader from './Loader/Loader';
+// import Button from './Button/Button';
 import { Component } from 'react';
-import s from './App.module.css'
-// export const App = () => {
-//   return (
-    // <div>
-    //   <Searchbar />
-    //   <Button />
-    // </div>
-//   );
-// };
+import s from './App.module.css';
 
 class App extends Component {
-  state = {}
 
-componentDidMount() { 
-  fetch()
- }
-
-  render(){ 
+  render() {
     return (
       <div className={s.App}>
-      <Searchbar />
-      <LoaderFn/>
-      <Button />
-    </div>
-    )
+        <Searchbar />
+
+        {/* {this.state.request && <ImageGallery />} */}
+        {/* <Loader />
+        <Button /> */}
+      </div>
+    );
   }
 }
-export default App
+export default App;
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -2,14 +2,41 @@ import { Component } from 'react';
 import s from './Searchbar.module.css';
 
 class Searchbar extends Component {
-  state = {};
+  state = {
+    request: null,
+    loading: false,
+    page: 1,
+    KEY: '28064028-9753e04b4800a7fc07442f07d',
+  };
+
+  componentDidMount() {
+    const { page, KEY, request } = this.state;
+    this.setState({ loading: true });
+    fetch(
+      `https://pixabay.com/api/?q=${request}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
+    )
+      .then(r => r.json())
+      .then(request => {
+        // console.log(request.hits);
+        this.setState({ request });
+      })
+      .finally(() => this.setState({ loading: false }));
+  }
+
+  submitForm = e => {
+    e.preventDefault();
+    this.setState({ request: '' });
+  };
+  changeInput = e => {
+    this.setState({ request: e.currentTarget.value.toLowerCase() });
+  };
 
   render() {
     return (
       <header className={s.Searchbar}>
-        <form className={s.SearchForm}>
+        <form className={s.SearchForm} onSubmit={this.submitForm}>
           <button type="submit" className={s.SearchForm_button}>
-          <span className={s.SearchForm_button_label}>Search</span>
+            <span className={s.SearchForm_button_label}>Search</span>
           </button>
 
           <input
@@ -18,6 +45,7 @@ class Searchbar extends Component {
             autoComplete="off"
             autoFocus
             placeholder="Search images and photos"
+            onChange={this.changeInput}
           />
         </form>
       </header>
