f1ded6bc3b582e198dd82a4db7bc2d20bc77e5a4
Volodymyr Bobanych <97664943+VollDeMarr@users.noreply.github.com>
2022-07-12 22:04:25 +0300

    test

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,9 @@
 import s from './Button.module.css';
-export default function Button(params) {
+export default function Button(onClick) {
+  console.log(onClick)
   return (
     <div className={s.container}>
-      <button type="button" className={s.btnMore}>
+      <button type="button" className={s.btnMore} onClick={this.loadMore}>
         Load more
       </button>
     </div>
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,21 +1,23 @@
 import { Component } from 'react';
 import Loader from '../Loader/Loader';
+import Button from '../Button/Button';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 import s from './ImageGallery.module.css';
 
 class ImageGallery extends Component {
   state = {
     request: '',
-    loading: false,
+    // loading: false,
     page: 1,
     KEY: '28064028-9753e04b4800a7fc07442f07d',
     error: null,
+    status: 'idle',
   };
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.requestName !== this.props.requestName) {
       const { page, KEY } = this.state;
       const { requestName } = this.props;
-      this.setState({ loading: true });
+      this.setState({ status: 'pending' });
       fetch(
         `https://pixabay.com/api/?q=${requestName}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
       )
@@ -23,27 +25,48 @@ class ImageGallery extends Component {
           if (response.ok) {
             return response.json();
           }
+          this.setState({ error: true });
           return Promise.reject(
             new Error('По даному запиту нічого не знайдено')
           );
         })
-        .then(request => this.setState({ request }))
-        .catch(error => this.setState({ error }))
+        .then(request => this.setState({ request, status: 'resolved' }))
+        .catch(error => this.setState({ error, status: 'rejected' }))
         .finally(() => this.setState({ loading: false }));
     }
   }
+  loadMore = () => {
+    this.setState(({ page }) => {
+      return {
+        page: page + 1,
+      };
+    });
+  };
+  
   render() {
-    const { request, loading, error } = this.state;
-    return (
-      <div>
-        {error && <h1>{error.message}</h1>}
-        <ul className={s.ImageGallery}>
-          <ImageGalleryItem prop={request} />
-        </ul>
-        {loading && <Loader />}
-        {/* <Loader/> */}
-      </div>
-    );
+    const { request, error, status } = this.state;
+    if (status === 'idle') {
+    }
+    if (status === 'pending') {
+      return (
+        <>
+          <Loader />{' '}
+        </>
+      );
+    }
+    if (status === 'rejected') {
+      return <h1>{error.message}</h1>;
+    }
+    if (status === 'resolved') {
+      return (
+        <>
+          <ul className={s.ImageGallery}>
+            <ImageGalleryItem prop={request} />
+          </ul>
+          <Button onClick={this.loadMore} />
+        </>
+      );
+    }
   }
 }
 
